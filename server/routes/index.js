68d2f66c388c22a8fb1c7f594fa2b0fd61517af7
carnio <carnioalessandro@gmail.com>
2020-12-25 18:30:48 +0000

    init

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const products=require('../products.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express',link:'/compra',user:'Alessandro',prodotto:'Scopa cm.80 4 ranghi' });
+  res.render('index', { title: 'Express',user:'Alessandro',articolo1:'Scopa cm.80 4 ranghi' });
 });
 
 router.get('/compra', function(req, res, next) {
   res.render('compra', { title: 'Truffa'});
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Registrazione', errore:''});
+});
+
+router.get('/product/:article/:color', function(req, res, next) {
+let articolo=req.params.article;
+let prodotto=products.find(el=>el.article==articolo);
+prodotto["color"]=req.params.color;
+res.render('product',prodotto);
 });
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('<h2>respond with a resource</h2>');
+});
+
+
+
+router.post('/accesso', function(req, res, next) {
+  let nome=req.body.nome;
+  let cognome=req.body.cognome;
+  let mail=req.body.mail;
+  let psw=req.body.password;
+  let rpsw=req.body.rpassword;
+  let sesso;
+  if (req.body.sesso=='maschio'){sesso='o'}else if (req.body.sesso=='femmina'){sesso='a'}else{sesso='i'};
+  if (psw==rpsw){res.render('accesso', { title: 'Benvenuto',nome:nome,finale:sesso });}
+  else{
+      res.render('signup', { title: 'Registrazione', errore:'Attenzione!! le password non coincidono'});
+  }
+
+  
 });
 
 module.exports = router;
